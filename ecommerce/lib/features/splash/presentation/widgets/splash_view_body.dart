8c202f545576c65dc04aade66c41da9a38691eb4
Meta-c <metamedhat116@gmail.com>
2022-11-15 12:57:56 +0200

    finished splash screen animation

--- ecommerce/lib/features/splash/presentation/widgets/splash_view_body.dart
+++ ecommerce/lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 
-class SplashBody extends StatelessWidget {
+class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
+
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+
+  Animation<double>? fadingAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+
+    animationController?.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +33,16 @@ class SplashBody extends StatelessWidget {
         // mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Spacer(),
-          const Text(
-            'Fruit Market',
-            style: TextStyle(
-                fontFamily: 'Poppins',
-                fontSize: 51,
-                fontWeight: FontWeight.bold,
-                color: Color(0xffffffff)),
+          FadeTransition(
+            opacity: fadingAnimation!,
+            child: const Text(
+              'Fruit Market',
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 51,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xffffffff)),
+            ),
           ),
           Image.asset('assets/images/splash_view_image.png')
         ],
--- ecommerce/lib/main.dart
+++ ecommerce/lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-
+import 'package:device_preview/device_preview.dart';
 import 'features/splash/presentation/splash_view.dart';
 
 void main() {
-  runApp(FruitsMarket());
+  runApp(DevicePreview(
+      enabled: !kReleaseMode, builder: (context) => const FruitsMarket()));
 }
 
 class FruitsMarket extends StatelessWidget {
@@ -13,8 +15,11 @@ class FruitsMarket extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home: SplashView(),
+      home: const SplashView(),
     );
   }
 }
